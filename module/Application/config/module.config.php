b6fcf3880365ecf67c7af5c9796540bf38525b59
Takashi Kawano <**>
2017-06-07 14:59:40 +0900

    fix route

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -15,7 +15,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Album\Controller\Index',
+                        'controller' => 'Album\Controller\Album',
                         'action'     => 'index',
                     ),
                 ),
