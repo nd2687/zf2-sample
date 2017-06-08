4ed4cd9d7d987c60c9ea78587342bd1a8ffecc0c
Takashi Kawano <**>
2017-06-08 14:43:04 +0900

    Introducing Services and the ServiceManager

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -1,28 +1,33 @@
 <?php
 
 return array(
-     'view_manager' => array(
-         'template_path_stack' => array(
-             __DIR__ . '/../view',
-         ),
-     ),
-    'controllers' => array(
-         'invokables' => array(
-             'Blog\Controller\List' => 'Blog\Controller\ListController'
-         )
-     ),
-    'router' => array(
-        'routes' => array(
-            'post' => array(
-                'type' => 'literal',
-                'options' => array(
-                    'route'    => '/blog',
-                    'defaults' => array(
-                        'controller' => 'Blog\Controller\List',
-                        'action'     => 'index',
-                    )
-                )
-            )
+    'service_manager' => array(
+        'invokables' => array(
+            'Blog\Service\PostServiceInterface' => 'Blog\Service\PostService'
         )
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    'controllers' => array(
+        'factories' => array(
+            'Blog\Controller\List' => 'Blog\Factory\ListControllerFactory'
+        )
+    ),
+    'router' => array(
+       'routes' => array(
+           'post' => array(
+               'type' => 'literal',
+               'options' => array(
+                   'route'    => '/blog',
+                   'defaults' => array(
+                       'controller' => 'Blog\Controller\List',
+                       'action'     => 'index',
+                   )
+               )
+           )
+       )
     )
 );
